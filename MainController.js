c5fbfad90bd8c0f74daa95faccb2b7089c89d2ac
warrlock608 <mackdan33@gmail.com>
2016-07-19 15:33:16 -0400

    Add files via upload

--- MainController.js
+++ MainController.js
@@ -2,12 +2,6 @@ app.controller('MainController', ['$scope', function($scope) {
     $scope.title = 'Dan\'s Music App';
     $scope.promo = 'First App Attempt';
 
-    $scope.plusOne = function(index) {
-        $scope.products[index].likes += 1;
-    };
-    $scope.minusOne = function(index) {
-        $scope.products[index].dislikes += 1;
-    };
     $scope.products = [
         {
             name: 'The Book of Trees',
