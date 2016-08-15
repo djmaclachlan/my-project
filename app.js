5e53246176965d6afe8f3fea482abc60f8e79791
warrlock608 <mackdan33@gmail.com>
2016-08-15 15:27:14 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -4,9 +4,6 @@
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
-        .when("/", {
-            templateUrl : "home.htm"
-        })
         .when("/red", {
             templateUrl : "red.htm"
         })
@@ -15,5 +12,33 @@ app.config(function($routeProvider) {
         })
         .when("/blue", {
             templateUrl : "blue.htm"
+        })
+        .otherwise({
+            templateUrl : "home.html",
+            controller  :  "MainController"
         });
 });
+app.controller('MainController', ['$scope', function($scope) {
+    $scope.title = 'Dan\'s web portfolio';
+    $scope.promo = 'Here are a list of some of my web projects';
+    $scope.projects = [
+        {
+            title: 'Action Telephone',
+            subtitle: 'Complete',
+            cover: 'carousel2.jpg',
+            link: 'http://www.actiontelephone.com'
+        },
+        {
+            title: 'Second Product',
+            subtitle: 'Ongoing',
+            cover: 'test.jpg',
+            link: 'http://warrlock608.github.io/my-project'
+        }
+    ];
+    $scope.plusOne = function(index) {
+        $scope.products[index].likes += 1;
+    };
+    $scope.minusOne = function(index) {
+        $scope.products[index].dislikes += 1;
+    };
+}]);
