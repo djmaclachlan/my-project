b8b14bf1f820c4a953ac71d020946cb2c33ee639
warrlock608 <mackdan33@gmail.com>
2016-08-15 15:07:28 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 /**
  * Created by NowWithLessVirus! on 7/19/16.
  */
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/index", {
+            templateUrl : "index.html"
+        })
+.when("/about", {
+    templateUrl : "aboutme.html"
+    })
+.when("/education", {
+    templateUrl : "education.html"
+    });
+});
