f1926c2e87e4b2351b0d9c92b05bfe78192ecc78
warrlock608 <mackdan33@gmail.com>
2016-08-31 17:44:35 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ app.config(function($routeProvider) {
 
         .when('/web', {
             templateUrl : 'web.html',
-            controller  : 'MainController',
+            controller  : 'MainController'
         })
 
         .when('/cpp', {
             templateUrl : 'cpp.html',
-            controller  : 'CPPController',
+            controller  : 'CPPController'
         })
 
         .otherwise({redirectTo: '/'});
@@ -25,76 +25,6 @@ app.config(function($routeProvider) {
 
 /*primary controller */
 app.controller('MainController', function($scope) {
-    $scope.title = 'Dan\'s web portfolio';
-    $scope.promo = 'Here is a list of some of my web projects';
-    $scope.projects = [
-        {
-            title: 'Action Telephone',
-            subtitle: 'Ongoing',
-            cover: 'carousel2.jpg',
-            link: 'http://www.actiontelephone.com'
-        },
-        {
-            title: 'Second Project',
-            subtitle: 'TBA',
-            cover: 'placeholder.png',
-            link: 'http://warrlock608.github.io/my-project'
-        },
-        {
-            title: 'Third Project',
-            subtitle: 'TBA',
-            cover: 'placeholder.png',
-            link: 'http://warrlock608.github.io/my-project'
-        }
-    ];
-    $scope.myName = 'Daniel J. MacLachlan';
-    $scope.myBio = 'Hello, my name is Dan MacLachlan, and welcome to my web portfolio. Within the contents of this page you will find past projects, current projects, and a few things that I hope to begin this year. My specialty is responsive design in a mobile environment and all work I do is mobile first. Over the course of the next year I intend to become very familiar with some of the more interactive javascript extensions such as Angular and Node.';
-    $scope.myPrimaryLanguages = 'HTML5, CSS3, Bootstrap, Javascript, Angularjs, PHP, and MySQL';
-    $scope.mySecondaryLanguages = 'C++, C#, Microsoft Batch Script';
-    $scope.myImage = 'me.jpeg';
-});
 
 
-/*Secondary Controller */
-app.controller('CPPController', function($scope) {
-    $scope.title = 'Dan\'s web portfolio';
-    $scope.promo = 'Here is a list of some of my C++ projects';
-    $scope.projects = [
-        {
-            title: 'Calculates Area of a Rectangle',
-            link: 'https://drive.google.com/file/d/0B8uAm7zc58m6WGhNby0xdEhidUU/view?usp=sharing'
-        },
-        {
-            title: 'Calculates the Average of Three Numbers',
-            link: 'https://drive.google.com/file/d/0B8uAm7zc58m6VFFvajM3NjNob1U/view?usp=sharing'
-        },
-        {
-            title: 'Converts Farenheit Into Celcius',
-            link: 'https://drive.google.com/file/d/0B8uAm7zc58m6VEhGMXNhalg4WEU/view?usp=sharing'
-        },
-        {
-            title: 'Calculates the Cost of Car Rental',
-            link: 'https://drive.google.com/file/d/0B8uAm7zc58m6ZFBGRkRYaEs5SFk/view?usp=sharing'
-        },
-        {
-            title: 'Calculates the Average of a Gradebook',
-            link: 'https://drive.google.com/file/d/0B8uAm7zc58m6bVJsMXVkUXc2V0E/view?usp=sharing'
-        },
-        {
-            title: 'Calculates the Distance Between Two Points',
-            link: 'https://drive.google.com/file/d/0B8uAm7zc58m6cE9nMEJYSDZ6ckU/view?usp=sharing'
-        },
-        {
-            title: 'Splits a Float Number Into A Whole Number and Fraction',
-            link: 'https://drive.google.com/file/d/0B8uAm7zc58m6SHZqTHFLRzdLMGs/view?usp=sharing'
-        },
-        {
-            title: 'Rounds a Float Number to a Designated Decimal Place',
-            link: 'https://drive.google.com/file/d/0B8uAm7zc58m6cEdSSnZuT2h3TUk/view?usp=sharing'
-        },
-        {
-            title: 'Calculates Total Cost of a Purchase Order',
-            link: 'https://drive.google.com/file/d/0B8uAm7zc58m6blF6Rzdhenp1T28/view?usp=sharing'
-        }
-    ];
 });
