e313151e17d4ed6c65ca358aed3b4131234f5d79
warrlock608 <mackdan33@gmail.com>
2016-09-07 23:44:24 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.controller('MainController', function($scope) {
     $scope.myName = "Daniel MacLachlan";
     $scope.myPrimaryLanguages = "HTML5, CSS5, PHP, MYSQL, Javascript, AngularJS";
     $scope.myBio = "Hello, and welcome to my web portfolio! I am an aspiring web developer living in Upstate, NY. Take a look around this site to get an idea of my work and style."
+    $scope.heroImage = {
+        background: 'url(appbg.jpg)'
+    };
 
 
 
