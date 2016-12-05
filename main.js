9d64d90cad38aeeec3509eaaeb6fa7caaeb72f74
warrlock608 <mackdan33@gmail.com>
2016-12-04 23:32:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Adolf In Space on 12/4/2016.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Write code here:
+var myDiv = <div className="left">This is the start a beautiful project</div>
+ReactDOM.render(
+    myDiv,
+    document.getElementById('app')
+);
