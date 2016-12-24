565199c1c6af62b0877217123c9b2d46d7e85662
warrlock608 <mackdan33@gmail.com>
2016-12-23 22:05:47 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
-/**
- * Created by NowWithLessVirus! on 7/19/16.
- */
-var app = angular.module('myApp', ['ngRoute']);
-app.config(function($routeProvider) {
-    $routeProvider
-
-        .when('/', {
-            templateUrl : 'home.html',
-            controller  : 'MainController'
-        })
-
-        .otherwise({redirectTo: '/'});
-});
-
-/*primary controller */
-app.controller('MainController', function($scope) {
-
-    $scope.myName = "Daniel MacLachlan";
-    $scope.myPrimaryLanguages = "HTML5, CSS5, PHP, MYSQL, Javascript, AngularJS";
-    $scope.myBio = "Hello, and welcome to my web portfolio! I am an aspiring web developer living in Upstate, NY. Take a look around this site to get an idea of my work and style."
-    $scope.heroImage = {
-        background: 'url(appbg.jpg)'
-    };
-
-
-
-
-
+$(document).ready(function(){
+    $("#red").click(function(){
+        $("#placeholder").removeClass("yellowLight");
+        $("#placeholder").removeClass("greenLight");
+        $("#placeholder").addClass("redLight");
+    });
+    $("#green").click(function(){
+        $("#placeholder").removeClass("redLight");
+        $("#placeholder").removeClass("yellowLight");
+        $("#placeholder").addClass("greenLight");
+    });
+    $("#yellow").click(function(){
+        $("#placeholder").removeClass("redLight");
+        $("#placeholder").removeClass("greenLight");
+        $("#placeholder").addClass("yellowLight");
+    });
 });
